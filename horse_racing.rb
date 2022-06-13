6f3819afdf5acca823176a91b86f0f3c2c02a69f
Thiemo Seys <thiemo.seys@nipro-group.com>
2022-06-13 09:38:34 +0200

    Added oneliner variant of horse racing algorithm

--- horse_racing.rb
+++ horse_racing.rb
@@ -44,7 +44,7 @@ class Course
 
   def race(horses)
     if horses.length > @capacity
-      raise ArgumentError, "There can not be more horses in a race than there is capacity for: #{horses.length} > #{@capacity} !"
+      raise ArgumentError, "There can not be more horses in a race than there is capacity for: horses > capacity / #{horses.length} > #{@capacity} !"
     else
       horses.sort.reverse
     end
@@ -52,8 +52,10 @@ class Course
 
 end
 
-def get_fastest_horse(horses, course)
+# not ideal that we mutate the horses list here!
+def get_fastest_horse!(horses, course)
   capacity = course.capacity
+  puts "Need #{horses.size / (capacity-1)} races to determine the fastest horse!"
   while horses.size > 1
     race_horses = horses[0..capacity - 1]
     race_order = course.race(race_horses)
@@ -63,9 +65,21 @@ def get_fastest_horse(horses, course)
   horses[0]
 end
 
-course = Course.new('Hippodroom', 3)
-#horse_names = (0...25).map{generate_random_string(10) }
-horse_names = ['schimmel', 'Bucephalus', 'paard1', 'paard2', 'paard3', 'paard4', 'paard5']
+course = Course.new('Hippodroom', 5)
+horse_names = (0...25).map{generate_random_string(10) }
+#horse_names = ['schimmel', 'Bucephalus', 'paard1', 'paard2', 'paard3', 'paard4', 'paard5']
 horses = horse_names.map { |name| Horse.new(name, generate_random_float(1, 10).round(2)) }
-horses.each {|horse| puts horse}
-puts "fastest horse is: #{get_fastest_horse(horses, course)}"
+puts "fastest horse is: #{get_fastest_horse!(horses, course)}"
+
+# Oneliner version
+# formula for amount of races for a given amount of horses X and a course with capacity Y
+# X/Y-1
+capacity = 5
+horses = (1...53).to_a
+amount_of_horses = horses.size
+amount_of_races = horses.size / (capacity-1)
+p "horses: #{horses}"
+p "amount of horses: #{amount_of_horses}"
+p "amount of races: #{amount_of_races}"
+fastest_horse = (0...amount_of_races).inject {|fastest_horse, race_iteration| horses[(race_iteration * capacity) - (race_iteration-1)..(race_iteration * capacity) + (capacity - race_iteration -1)].push(fastest_horse).sort.reverse[0]}
+puts "fastest horse is #{fastest_horse}"
